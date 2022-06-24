9cca74f8481c25d248b817d80374f2d535ecf6b2
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-06-24 19:22:33 +0200

    Update shell config

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python3
+    pkgs.python3.pkgs.pip
+    pkgs.python3.pkgs.setuptools
+    pkgs.python3.pkgs.wheel
+  ];
+  shellHook = ''
+    export PIP_PREFIX=$(pwd)/.pip
+    export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    export PYTHONPATH="$(pwd):$PYTHONPATH"
+    unset SOURCE_DATE_EPOCH
+  '';
+}
