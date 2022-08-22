513ed994797e02b089f8fbbffb16c37f9fae43a5
Radosław Szamszur <radoslawszamszur@gmail.com>
2022-08-22 19:35:00 +0200

    Add module __all__ attribute

--- {{cookiecutter.folder_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.folder_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,8 +1,10 @@
 """{{cookiecutter.project_description}}"""
 import logging
 
-from .version import __version__  # noqa: F401
+from .version import __version__
 
 # initialize logging
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
+
+__all__ = ("__version__",)
